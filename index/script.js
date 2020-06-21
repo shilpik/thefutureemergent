91fbf64387c710c4b44b1b83e7e2c2630bd581fc
shilpik <shilpi.lal.kumar@gmail.com>
2020-06-21 15:54:13 -0700

    Janga Session
    Circles! Fonts! Hover!

--- index/script.js
+++ index/script.js
@@ -1,9 +1,58 @@
-function setup(){
-  createCanvas(1280,1024);
-  backgroundColor(inherit);
+function setup() {
+  createCanvas(1000, 1000, WEBGL);
+  img = loadImage('tiare1.png');
 }
 
-function draw(){
-  ellipses(w,y,50,50);
-  color(white);
+function draw() {
+  background(0,0,0);
+
+  translate(-240, -100, 0);
+  normalMaterial();
+  surface(img);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(70);
+  pop();
+
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  sphere(i);
+  pop();
+
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cylinder(70, 70);
+  pop();
+
+  translate(-240 * 3, 200, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cone(70, 70);
+  pop();
+
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(70, 20);
+  pop();
+
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  sphere(70);
+  pop();
 }
