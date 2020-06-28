d7541e54934ce44d65053e1c8cd770b182dad7c1
shilpik <shilpi.lal.kumar@gmail.com>
2020-06-28 14:57:42 -0700

    big musings session

--- index/logbook.js
+++ index/logbook.js
@@ -1,6 +1,27 @@
-function myFunction() {
+function myFunctionamy() {
   var x =
-  document.getElementById("panel")
+  document.getElementById("panelamy")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionalicia() {
+  var x =
+  document.getElementById("panelalicia")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunctionkytana() {
+  var x =
+  document.getElementById("panelkytana")
 
   if (x.style.display === "block") {
     x.style.display = "none";
