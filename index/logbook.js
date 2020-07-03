6ec6639c7f503a0d78a611ffe81b353ada1faefe
shilpik <shilpi.lal.kumar@gmail.com>
2020-07-03 09:35:52 -0700

    Lets try this

--- index/logbook.js
+++ index/logbook.js
@@ -18,10 +18,101 @@ function myFunctionalicia() {
     x.style.display = "block";
   }
 }
+function myFunctionartalicia() {
+  var x =
+  document.getElementById("panelartalicia")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
 function myFunctionkytana() {
   var x =
   document.getElementById("panelkytana")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionartkytana() {
+  var x =
+  document.getElementById("panelartkytana")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionartamy() {
+  var x =
+  document.getElementById("panelartamy")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunctionartmalik() {
+  var x =
+  document.getElementById("panelartmalik")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionartblr() {
+  var x =
+  document.getElementById("panelartblr")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctiondelta() {
+  var x =
+  document.getElementById("paneldelta")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionartdelta() {
+  var x =
+  document.getElementById("panelartdelta")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionarttiare() {
+  var x =
+  document.getElementById("panelarttiare")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionartserena() {
+  var x =
+  document.getElementById("panelartserena")
 
   if (x.style.display === "block") {
     x.style.display = "none";
